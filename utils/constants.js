766fc993abd671912337b776a5d2e5a85dc77300
Pavel Melekhin <79168445519@yandex.ru>
2023-03-17 18:24:35 +0300

    sprint14 v0.1

--- utils/constants.js
+++ utils/constants.js
@@ -1,2 +1,2 @@
 module.exports.SOME_SECRET_KEY = 'some_secret_key';
-module.exports.REGEX_LINK = /(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/;
+module.exports.REGEX_LINK = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
